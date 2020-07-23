e047d37ada3060704409645e50df26f21720e902
wuzhonglong <agony123456>
2020-07-23 14:53:24 +0800

    第一次提交

---
+++ src/main/java/com/wzl/bootsso/BootSsoApplication.java
@@ -0,0 +1,13 @@
+package com.wzl.bootsso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootSsoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootSsoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wzl/bootsso/BootSsoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wzl.bootsso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootSsoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
